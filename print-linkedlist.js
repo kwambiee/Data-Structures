a89508e5e7645b79a23ccd1d8abb94bc00fa7adc
Joyous Kwambiee <kwambokaj2.jk@gmail.com>
2022-11-02 07:43:28 +0300

    Insert data to the last position

--- print-linkedlist.js
+++ print-linkedlist.js
@@ -16,6 +16,27 @@ class LinkedList{
         this.head = new Node(data, this.head);
         this.size ++;
     }
+    // insert node at last position
+    insertLast(data){
+        // create node
+        let node = new Node(data);
+        let current;
+        // if empty make it head
+        if(!this.head){
+            this.head = node;
+        }else{
+            // make current head
+            current = this.head;
+            // traverse through the list
+            while(current.next){
+                current = current.next;
+            }
+            // assign node to the end
+            current.next = node;
+        }
+        // increase size
+        this.size++;
+    }
 
     // print the list
     printListData(){
@@ -30,5 +51,7 @@ let list = new LinkedList();
 list.insertFirst(100);
 list.insertFirst(200);
 list.insertFirst(300);
+list.insertLast(400);
+list.insertLast(500);
 
 list.printListData();
