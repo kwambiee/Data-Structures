734dad04f1e3ca8c23bd3676d63d564736ae7854
Joyous Kwambiee <kwambokaj2.jk@gmail.com>
2022-11-21 12:38:48 +0300

    Add commentaries to code

--- diagnol-difference.js
+++ diagnol-difference.js
@@ -12,7 +12,9 @@ function diagonalDifference(arr){
   let rightDiag = 0;
   let result = 0;
   for(let i=0; i<arr.length; i++){
+    // it will start with arr[0][0] as it increases to arr[1][1]
     leftDiag += arr[i][i];
+    // it will start with the first array, pick the last element of the array and subtract according to loop.
     rightDiag += arr[i][arr.length-1-i];
   }
   result = leftDiag - rightDiag;
